ab0554235aff450dcce244a8088c9cfe41216944
Thiago Cardoso <thiagocardoso0806@gmail.com>
2025-01-28 10:45:54 -0300

    Update sorteio.js

--- js/sorteio.js
+++ js/sorteio.js
@@ -17,7 +17,14 @@ function write(){
   const texto = random();
   const link = "Txt/Livros/Literatura/" + texto + ".zip"
   const container = document.getElementById("container");
-  container.innerHTML = `<a href="${link}"><h1>${texto}</h1><a>`;
+  const hoje = new Date();
+  const ano = hoje.getFullYear();
+  const mes = String(hoje.getMonth() + 1).padStart(2, '0'); // Adiciona zero à esquerda
+  const dia = String(hoje.getDate()).padStart(2, '0');
+  const dataFormatada = `${dia}/${mes}/${ano}`;
+  container.innerHTML = `<a href="${link}"><h1>${texto}</h1><a><p class="data">${dataFormatada}</p>`;
 }
+
+
 
 write()
