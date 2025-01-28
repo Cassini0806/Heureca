07db0e312bc1cc544efe0032c845a28b2f1edaf2
Thiago Cardoso <thiagocardoso0806@gmail.com>
2025-01-28 10:34:24 -0300

    Add files via upload

--- js/sorteio.js
+++ js/sorteio.js
@@ -1,21 +1,23 @@
-function openPDF() {
-  var links = [
-              "google.com",
-              "youtube.com",
-              "reddit.com",
-              "apple.com"]
-              
-  
-              openPDF = function() {
-                // get a random number between 0 and the number of links
-                var randIdx = Math.random() * links.length;
-                // round it, so it can be used as array index
-                randIdx = parseInt(randIdx, 10);
-                // construct the link to be opened
-                var link = 'http://' + links[randIdx];
-                };
-                
-      return link;
-      
-      document.getElementById("link").innerHTML = openSite();
-  }
+function random(){
+  var livros = [
+      "Chamado-de-Cthulhu",
+      "Dom-Casmurro",
+      "Frankenstein",
+      "Guerra-dos-Mundos",
+      "Um-Estudo-em-Vermelho",
+      "Vidas-Secas",
+      "pinocchio",
+  ]
+  const title = livros[Math.floor(Math.random() * (6 - 0 + 1))] 
+  console.log(title)
+  return title
+}
+
+function write(){
+  const texto = random();
+  const link = "Txt/Livros/Literatura/" + texto + ".zip"
+  const container = document.getElementById("container");
+  container.innerHTML = `<a href="${link}"><h1>${texto}</h1><a>`;
+}
+
+write()
